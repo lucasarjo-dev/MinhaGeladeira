e74da6f43b721a431202406f4ba08c5d83300dee
lucasarjo-dev <lucaspao430@gmail.com>
2026-08-01 21:01:22 -0300

    Change database connection parameters
    Updated database connection details for MySQL.

--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -1,9 +1,9 @@
 package org.example.database;
 import java.sql.*;
 public class DatabaseConnection {
-private static final String url = "jdbc:mysql://127.0.0.1:3306/myfridge?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-private static final String user = "root";
-private static final String password = "Nightreign2";
+private static final String url = "jdbc:mysql://127.0.0.1:3306/MinhaGeladeira?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+private static final String user = "";
+private static final String password = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
